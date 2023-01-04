99e8939c00d43cb1380045da37a131cf78d8b468
jeong-hyuk <jack1150@naver.com>
2023-01-04 23:34:33 +0900

    Feat: "추가 프로그래머스 120812 최빈값 구하기"

---
+++ 프로그래머스/120812.java
@@ -0,0 +1,23 @@
+//프로그래머스 No.120812 최빈값 구하기
+
+class Solution {
+    public int solution(int[] array) {
+        int answer = -1;
+        int[] num = new int[2000];
+        for(int i = 0; i < array.length; i++){
+            num[array[i] + 1000]++;
+        }
+
+        int max = 0;
+        for(int i = 0; i < 2000; i++){
+            if(num[i] > max){
+                max = num[i];
+                answer = i - 1000;
+            }
+            else if(num[i] == max){
+                answer = -1;
+            }
+        }
+        return answer;
+    }
+}
