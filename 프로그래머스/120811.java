ef509749e36650cbca4d8158b735d768ef496bb0
jeong-hyuk <jack1150@naver.com>
2023-01-05 23:42:56 +0900

    Feat: "추가 프로그래머스 120811 중앙값 구하기"

---
+++ 프로그래머스/120811.java
@@ -0,0 +1,15 @@
+//프로그래머스 No. 120811 중앙값 구하기
+
+import java.util.*;
+
+class Solution {
+    public int solution(int[] array) {
+        int answer = 0;
+
+        Arrays.sort(array);
+
+        answer = array[array.length/2];
+
+        return answer;
+    }
+}
