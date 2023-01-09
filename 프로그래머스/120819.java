9aabec70051d1cade20909848c59ebfa71e26bba
jeong-hyuk <jack1150@naver.com>
2023-01-09 23:14:14 +0900

    Feat: "추가 프로그래머스 120819 아이스 아메리카노"

---
+++ 프로그래머스/120819.java
@@ -0,0 +1,10 @@
+//프로그래머스 No.120819 아이스 아메리카노
+
+class Solution {
+    public int[] solution(int money) {
+        int[] answer = new int[2];
+        answer[0] = money/5500;
+        answer[1] = money%5500;
+        return answer;
+    }
+}
