7c6ece9b39e75dc3c5364d174245a1f0e7097ad3
jeong-hyuk <jack1150@naver.com>
2023-01-09 23:11:01 +0900

    Feat: "추가 프로그래머스 120818 옷가게 할인 받기"

---
+++ 프로그래머스/120818.java
@@ -0,0 +1,19 @@
+//프로그래머스 No.120818 옷가게 할인 받기
+
+class Solution {
+    public int solution(int price) {
+        if (price >= 500000) {
+            return (int)(price * 0.8);
+        }
+
+        if (price >= 300000) {
+            return (int)(price * 0.9);
+        }
+
+        if (price >= 100000) {
+            return (int)(price * 0.95);
+        }
+
+        return price;
+    }
+}
